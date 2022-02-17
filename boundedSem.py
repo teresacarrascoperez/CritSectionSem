627246e20e02da72f884e1de652ededc0f6a3327
teresacarrascoperez <terescar@ucm.es>
2022-02-17 19:10:10 +0100

    boundedSem

---
+++ boundedSem.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 10 10:21:35 2022
+
+@author: teresa
+"""
+
+from multiprocessing import Process, BoundedSemaphore
+from multiprocessing import current_process
+from multiprocessing import Value, Array
+
+
+N = 8
+def task(common, tid, semaphore):
+    semaphore.acquire() #está esperando para entrar
+    try:
+        a = 0
+        for i in range(100):
+            print(f"{tid}−{i}: Non−critical Section")
+            a += 1
+            print(f"{tid}−{i}: End of non−critical Section")
+            print(f"{tid}−{i}: Critical section")
+            v = common.value + 1
+            print(f"{tid}−{i}: Inside critical section")
+            common.value = v
+            print(f"{tid}−{i}: End of critical section")
+    finally:
+        semaphore.release()
+
+def main():
+    lp = []
+    common = Value('i', 0)
+    semaphore = BoundedSemaphore(1)
+    for tid in range(N):
+        lp.append(Process(target=task, args=(common, tid, semaphore)))
+    print (f"Valor inicial del contador {common.value}")
+    for p in lp:
+        p.start()
+    
+    for p in lp:
+        p.join()
+    
+    print (f"Valor final del contador {common.value}")
+    print ("fin")
+
+if __name__ == "__main__":
+    main()
